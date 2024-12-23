7cd8cad39074a2b43d52c52ae46e9f570931d0bb
Byron Villegas Moya <byronvillegasm@gmail.com>
2024-12-23 20:02:35 -0300

    Changed: prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   server: {
-    url: "https://ms-discs.onrender.com/api",
+    url: "https://ms-discs.vercel.app/api",
     paths: {
       discs: "discs"
     }
