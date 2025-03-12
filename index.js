f7a068a614ab10a50b6b045220b11bb0bd96bf0b
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-03-12 00:31:38 -0300

    Fix vercel config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const angularConfig = require('./angular.json');
 
 const app = express();
 
-const baseHref = angularConfig.projects['fe-discs'].architect.build.options.baseHref;
+const basePath = angularConfig.projects['fe-discs'].architect.build.options.baseHref;
 const outputPath = angularConfig.projects['fe-discs'].architect.build.options.outputPath;
 
 const buildPath = '/' + outputPath;
@@ -12,18 +12,14 @@ const buildPath = '/' + outputPath;
 const indexFile = 'index.html';
 const indexFilePath = buildPath + '/' + indexFile;
 
-const basePath = baseHref.substring(0, baseHref.indexOf('/'));
-
-const appPath = baseHref + '/**';
-
 app.use(basePath, express.static(__dirname + buildPath)); // Para la ruta genera archivos estaticos
 
-app.get(appPath, (req, res) => { // Al entrar en la ruta base
+app.get(basePath, (req, res) => { // Al entrar en la ruta base
     res.sendFile(path.join(__dirname + indexFilePath));  // Retorna el index.html
 });
 
 const PORT = process.env.PORT || angularConfig.projects['fe-discs'].architect.serve.options.port;
-const PATH = baseHref;
+const PATH = basePath.substring(0, basePath.lastIndexOf('/'));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}${PATH}`);
