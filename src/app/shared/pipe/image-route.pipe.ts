b0a4e12bf639ed5760bdedd0b727563eddb4473d
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-03-21 04:38:31 -0300

    Changed angular.json and image-route pipe

--- src/app/shared/pipe/image-route.pipe.ts
+++ src/app/shared/pipe/image-route.pipe.ts
@@ -7,6 +7,6 @@ import { Disc } from 'src/app/core/models/disc';
 export class ImageRoutePipe implements PipeTransform {
 
   transform(disc: Disc, image: string): any {
-    return 'assets/images/' + disc.type.toLowerCase() + '/' + disc.sku + '/' + image;
+    return 'assets/images/discs/' + disc.type.toLowerCase() + '/' + disc.sku + '/' + image;
   }
 }
